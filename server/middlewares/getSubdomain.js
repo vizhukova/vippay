d53b436dd877be7da1430be521566fca68046756
andrew <kalipsso96@gmail.com>
2016-02-04 14:03:38 +0200

    hotfix domain

--- server/middlewares/getSubdomain.js
+++ server/middlewares/getSubdomain.js
@@ -1,8 +1,10 @@
+var config = require('./../config');
+
 module.exports = function(req, res, next){
 
     var domain = req.get('host');
     req.subdomain = domain.split('.')[0];
-    req.postdomain = 'vippay.loc';
+    req.postdomain = config.get('domain');
     req.hostname = domain;
     next();
 
