fa6d79d31bc4e339a35565c46447dcf1d81b0ba6
andrew <kalipsso96@gmail.com>
2016-02-04 18:15:22 +0200

    redirect fix

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var SettingsController = require('../controllers/Settings');
 var RateController = require('../controllers/Rate');
+var _ = require('lodash');
 
 
 router.get('/settings', function(req, res){
 
-
-    res.send({link: `/partners/${req.clientObj.id}`});
+    var link = `http://${req.subdomain}.${req.postdomain}/partners`;
+    res.send({link: link});
 
 });
 
@@ -49,6 +50,28 @@ router.put('/fee', function(req, res) {
     SettingsController.editFee({client_id: req.user.id, fee: req.body.fee})
             .then(function(fee){
                 res.send(fee);
+            }).catch(function(err) {
+                res.status(400).send(err.errors);
+            });
+
+});
+
+router.get('/payment', function(req, res) {
+
+    SettingsController.getPayment(req.user.id)
+            .then(function(data){
+                res.send(data.payment);
+            }).catch(function(err) {
+                res.status(400).send(err.errors);
+            });
+
+});
+
+router.put('/payment', function(req, res) {
+
+    SettingsController.putPayment({payment: req.body, user_id: req.user.id})
+            .then(function(payment){
+                res.send(payment);
             }).catch(function(err) {
                 res.status(400).send(err.errors);
             });
