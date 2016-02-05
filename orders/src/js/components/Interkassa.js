69f31434b0ac883847405b421629a967c34e6c36
andrew <kalipsso96@gmail.com>
2016-02-05 17:44:28 +0200

    redirect to another doment and enter

---
+++ orders/src/js/components/Interkassa.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import OrdersStore from'./../stores/OrdersStore'
+import OrderActions from'./../actions/OrdersActions'
+
+class Order extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return <div className="panel panel-default col-md-10 col-md-offset-1 form-margin">
+                 <form id="payment" name="payment" method="post" action="https://sci.interkassa.com/" enctype="utf-8">
+                     <input  name="ik_co_id" type="hidden" value={this.props.method.ik_co_id} />
+                     <input  name="ik_pm_no" type="hidden" value={this.props.method.ik_pm_no} />
+                     <input  name="ik_am" type="hidden" value={this.props.method.ik_am} />
+                     <input  name="ik_cur" type="hidden" value={this.props.method.ik_cur} />
+                     <input  name="ik_desc" type="hidden" value={this.props.method.ik_desc} />
+
+                        <input type="submit" className="btn btn-danger btn-lg" value="Pay" />
+                </form>
+                </div>
+    }
+}
+
+//type="hidden"
+
+export default Order;
