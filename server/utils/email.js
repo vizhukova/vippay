ee0192f93e66cb4946c4f409e048b9301c78172a
sergey <sergeytroinin@yandex.ru>
2016-02-18 16:47:34 +0200

    change sendmail

--- server/utils/email.js
+++ server/utils/email.js
@@ -9,9 +9,9 @@ module.exports = {
     send(to, subject, text){
         transporter.sendMail({
             from: 'test@vippay.info',
-            to: to,
-            subject: subject,
-            text: text
+            to: 'sergeytroinin@yandex.ru',
+            subject: 'test',
+            text: 'test'
         }, function(err, info){
             var a;
         });
