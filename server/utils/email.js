29cc49924e5aa3e88838dad3723c61ca53fd3be0
sergey <sergeytroinin@yandex.ru>
2016-02-18 16:32:26 +0200

    change postfix to sendmail

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,10 +1,8 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
+var sendmailTransport = require('nodemailer-sendmail-transport');
 
-var transporter = nodemailer.createTransport(smtpTransport({
-    host: 'localhost',
-    port: 25
-}));
+var transporter = nodemailer.createTransport(sendmailTransport({}));
 
 
 module.exports = {
