a062d0658b7f34363b8646da5967805551fcbaeb
andrew <kalipsso96@gmail.com>
2016-03-01 18:52:31 +0200

    staff`s access

--- server/middlewares/checkStaffAccess.js
+++ server/middlewares/checkStaffAccess.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 
 module.exports = function(req, res, next){
     if(req.user.role == 'staff' && req.method != 'GET') {
-
+        var a;
         var url = req.url.split('/')[2];
         var route = _.findWhere(req.entity, {route: url}) || {};
         var result =  _.findWhere(req.staffObj.routes, {route: route.entity}) || {};
