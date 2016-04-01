314c8babfdd1410972288352b556f150f525b5ed
andrew <kalipsso96@gmail.com>
2016-04-01 11:43:17 +0300

    added files to gitignore

---
+++ server/public/orders/js/paymentSettings.js
@@ -0,0 +1,23 @@
+var paymentTranslate = {
+
+    "interkassa": "Интеркасса",
+    "id_kassa": "Номер кассы",
+    "id_confirm":"Номер подтверждения",
+    "interkassa_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "interkassa_more_info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+
+    "yandex": "Яндекс",
+    "receiver": "Номер яндекс кошелека",
+    "yandex_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "yandex_more_info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+
+    "pay_after_receive": 'Наложный платеж',
+    "pay_after_receive_details": "Оплата при  получении",
+
+    "send_to_card": "Банковский перевод",
+    "send_to_card_details": "Мы отошлем вам товар после получения оплаты на карту",
+
+    "western_union": "Western Union",
+    "western_union_details": "Мы отошлем вам товар после получения перевода"
+
+};
