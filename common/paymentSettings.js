a3b6d27c760e1dd5637094f5ff1938fcb44764a5
Zhukova Veronika <veronikazhukova9@gmail.com>
2016-06-21 18:12:23 +0300

    fix

--- common/paymentSettings.js
+++ common/paymentSettings.js
@@ -26,6 +26,11 @@ export default {
     "liqpay_details": "Введите данные о платежной системе",
     "liqpay_more_info": "Скопируйте ваш публичный и приватный ключи в соответствующие поля",
 
+     "paypal": "Paypal",
+    "client_id": "clientId",
+    "client_secret": "clientSecret",
+    "paypal_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "paypal_more_info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 
     "pay_after_receive": 'Наложный платеж',
     "pay_after_receive_details": "Оплата при  получении",
