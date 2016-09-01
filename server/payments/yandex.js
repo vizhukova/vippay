e5925dd89d7fdaf1ecca924475b68b1b5d664bc8
sergey <sergeytroinin@yandex.ru>
2016-09-01 12:38:10 +0300

    fix yandex wallet

--- server/payments/yandex.js
+++ server/payments/yandex.js
@@ -51,7 +51,7 @@ class YandexMoney {
                 
                 var product = order.product[0];
 
-                if(product.currency_id !== 4){
+                if(product.currency_id !== 4 && order.basic_currency_id !== 4){
                     return Rate.getResult({
                         client_id: order.client_id,
                         from: order.basic_currency_id,
